cdbe752da62bd35c05a3a5f344f9f81aa13e1125
Dan Rees-Jones <revdan@gmail.com>
2023-03-12 14:10:01 +0000

    create placeholder Project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_12_135910) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_140648) do
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
