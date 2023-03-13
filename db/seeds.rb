f0988d2a243b4c80baac7d10613c943ffc00f86b
Dan Rees-Jones <revdan@gmail.com>
2023-03-13 10:54:15 +0000

    add authentication to all pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
+Project.delete_all
+User.delete_all
+
 Project.create({name: "Project 1"})
+User.create! name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password"
