93d124fc515da4776b8126e6e3f824356eefc64e
Dan Rees-Jones <revdan@gmail.com>
2023-03-13 11:40:39 +0000

    add a failing test for Project#show

---
+++ spec/factories/project.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :project do
+    sequence :name do |n|
+       "Project #{n}"
+    end
+  end
+end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Projects", type: :request do
+  let(:user) { create(:user) }
+
   describe "GET /index" do
-    let(:user) { create(:user) }
 
     it "requires user to be authenticated" do
       sign_in user
@@ -13,6 +14,27 @@ RSpec.describe "Projects", type: :request do
 
     it "redirects if user is not authenticated" do
       get root_path
+
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe "SHOW /index" do
+    let(:project) { create(:project) }
+
+    describe "when user is signed in" do
+      before do 
+        sign_in user
+        get project_path(project)
+      end
+
+      it "displays the correct page" do
+        expect(response).to render_template(:show)
+      end
+    end
+
+    it "redirects if user is not authenticated" do
+      get project_path(project)
 
       expect(response).to redirect_to(new_user_session_path)
     end
