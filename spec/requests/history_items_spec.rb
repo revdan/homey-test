af1c79d5d26b523b34e270439c2659299d14b67e
Dan Rees-Jones <revdan@gmail.com>
2023-03-14 12:29:32 +0000

    Add failing test for HistoryItem#post

--- spec/requests/history_items_spec.rb
+++ spec/requests/history_items_spec.rb
@@ -1,7 +1,43 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "HistoryItems", type: :request do
+  let(:user) { create(:user) }
+  let(:project) { create(:project) }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "requires user to be authenticated" do
+      sign_in user
+      get project_history_items_path(project)
+
+      expect(response).to render_template(:index)
+    end
+
+    it "redirects if user is not authenticated" do
+      get project_history_items_path(project)
+
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe "POST /index" do
+    before { sign_in user }
+
+    it "posts comments" do
+      expect {
+        post project_history_items_path(project.id, format: :turbo_stream), params: {project_id: project.id, history_item: {type: "HistoryComment", body: "sdffd dsf  sdf"}}
+      }.to change { HistoryItem.count }.by(1)
+    end
+
+    it "posts status changes" do
+      expect {
+        post project_history_items_path(project.id, format: :turbo_stream), params: {project_id: project.id, history_item: {type: "HistoryStatusChange", body: "sdffd dsf  sdf"}}
+      }.to change { HistoryItem.count }.by(1)
+    end
+
+    it "doesn't post unknown types" do
+      expect {
+        post project_history_items_path(project.id, format: :turbo_stream), params: {project_id: project.id, history_item: {type: "RandomComment", body: "sdffd dsf  sdf"}}
+      }.to raise_error(ActiveRecord::SubclassNotFound)
+    end
   end
 end
