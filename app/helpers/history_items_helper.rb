39aa43ed9b8471d137481873822c4e9a4f209f96
Dan Rees-Jones <revdan@gmail.com>
2023-03-14 12:32:50 +0000

    Allow new HistoryItems to be added to list

--- app/helpers/history_items_helper.rb
+++
@@ -1,2 +0,0 @@
-module HistoryItemsHelper
-end
--- app/models/history_item.rb
+++ app/models/history_item.rb
@@ -2,5 +2,5 @@ class HistoryItem < ApplicationRecord
   belongs_to :project
   has_one :user
 
-
+  scope :ordered, -> { order(created_at: :desc) }
 end
