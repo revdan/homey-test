2150f0c83a8c4d3eb13f15cc6872a2e353e6fe3a
Dan Rees-Jones <revdan@gmail.com>
2023-03-15 15:03:33 +0000

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.delete_all
 HistoryItem.delete_all
 
 p = Project.create(name: "Project 1")
+p2 = Project.create(name: "Project 2")
 u = User.create!(name: "John Doe", email: "john@example.com", password: "password", password_confirmation: "password")
+u2 = User.create!(name: "Don Joe", email: "don@example.com", password: "password", password_confirmation: "password")
 
 HistoryComment.create(project_id: p.id, body: "My first comment", user_id: u.id)
-HistoryStatusChange.create(project_id: p.id, body: "My first status change", user_id: u.id)
+HistoryStatusChange.create(project_id: p.id, body: "My first status change", user_id: u2.id)
